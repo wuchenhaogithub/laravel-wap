7e8da567f9c379716813aa58537fdf52a943b06e
武晨昊 <18310828498@163.com>
2021-04-13 09:40:50 +0800

    wap-artisan

---
+++ src/Data/Goods/Http/Controllers/GoodsController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Wuchenhao\LaravelShop\Data\Goods\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Wuchenhao\LaravelShopHttp\Controllers\Controller;
+
+class GoodsController extends Controller
+{
+    //
+}
---
+++ src/Data/Goods/Models/Goods.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Wuchenhao\LaravelShop\Data\Goods\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Goods extends Model
+{
+    //
+}
